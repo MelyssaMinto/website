71ff9c2ce150eada16725b64281e6f131393805c
MelyssaMinto <msm110@duke.edu>
2021-12-24 00:39:23 -0500

    major update

---
+++ nav.js
@@ -0,0 +1,11 @@
+// <!-- JS Function to have a responsive nav-->
+
+
+    function myFunction() {
+      var x = document.getElementById("myTopnav");
+      if (x.className === "topnav") {
+        x.className += " responsive";
+      } else {
+        x.className = "topnav";
+      }
+    }
